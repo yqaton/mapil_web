de28fabc2438fd00189a6d5968fa60148731e622
rbsx <ignat@timepad.ru>
2017-12-17 21:57:27 +0300

    wutta

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-const Hello = () => (
-  <div>
-    Hello from react
-  </div>
+import 'semantic-ui-css/semantic.min.css'
+import { Grid, Input, Container, Header, Divider } from 'semantic-ui-react'
+
+const App = () => (
+  <Grid container style={{ padding: '5em 0em' }}>
+    <Container
+      textAlign='center'
+    >
+      <Header as='h1'>Mapil - Admin panel</Header>
+      <Divider horizontal='true' />
+      <Input
+        action={
+          {
+            color: 'teal',
+            labelPosition: 'right',
+            icon: 'copy',
+            content: 'Copy'
+          }
+        }
+        defaultValue='http://gl.lebga.yy/prob'
+      />
+    </Container>
+  </Grid>
 )
 
-export default Hello
 
-render(<Hello />, document.getElementById('app'));
+render(
+  <App />,
+  document.getElementById('app')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -21,15 +21,33 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/, // Кроме нодмоудльс
         use: ['babel-loader']
-      }
+      }, {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }, {
+        test: /\.less$/,
+        use: [{
+          loader: 'style-loader' // creates style nodes from JS strings
+        }, {
+          loader: 'css-loader' // translates CSS into CommonJS
+        }, {
+          loader: 'less-loader' // compiles Less to CSS
+        }]
+      },
+      { test: /\.svg$/, loader: 'url-loader?limit=65000&mimetype=image/svg+xml&name=public/fonts/[name].[ext]' },
+      { test: /\.woff$/, loader: 'url-loader?limit=65000&mimetype=application/font-woff&name=public/fonts/[name].[ext]' },
+      { test: /\.woff2$/, loader: 'url-loader?limit=65000&mimetype=application/font-woff2&name=public/fonts/[name].[ext]' },
+      { test: /\.[ot]tf$/, loader: 'url-loader?limit=65000&mimetype=application/octet-stream&name=public/fonts/[name].[ext]' },
+      { test: /\.eot$/, loader: 'url-loader?limit=65000&mimetype=application/vnd.ms-fontobject&name=public/fonts/[name].[ext]' },
+      { test: /\.png$/, loader: 'url-loader?limit=65000&mimetype=image/svg+xml&name=public/fonts/[name].[ext]'}
     ]
   },
-  // Автоматически резолвим js и jsx расширения, 
+  // Автоматически резолвим js и jsx расширения,
   // чтоб писать include './App'
   resolve: {
-    extensions: ['.js','.jsx']
+    extensions: ['.js', '.jsx']
   },
-
+  devtool: 'eval-source-map',
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(paths.SRC, 'index.html')
